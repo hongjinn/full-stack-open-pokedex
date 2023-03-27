e0c7bac479dd4e6fbccd86d753c9b5a35cdcfa5d
hongjinn <hongjinnpark@gmail.com>
2023-03-26 21:36:16 -0700

    added tagline text

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const PokemonList = ({ pokemonList }) => {
   return (
     <div>
-      <h1 style={{ marginLeft: '15px' }}>Pokedex</h1>
+      <h1 style={{ marginLeft: '15px' }}>Pokedex - gotta catch em all</h1>
       <div className="list-container">
         {pokemonList.map(({ id, name }) => (
           <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
